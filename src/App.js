e73852d2492e3cd8f08ed77f4b93de7b2b02dbb9
Dilshan Wickramasinghe <dilshanmw717@gmail.com>
2024-04-30 20:16:24 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Provider } from "react-redux";
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import Footer from "./components/Footer";
+import ViewWeather from "./pages/ViewWeather";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { store, persistor } from "../src/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <div className="my_app">
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route exact path="/DashBoard" element={<Dashboard />} />
+              <Route exact path="/:CityCode" element={<ViewWeather />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default App;
